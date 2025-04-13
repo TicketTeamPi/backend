791ea9c64830bcaefd0403203b999a2e3502f2ee
GabrielFogo <gabrielcostal164@gmail.com>
2025-04-13 12:49:01 -0300

    feat: crate prisma service

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,57 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import antfu from '@antfu/eslint-config';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+export default antfu({
+  stylistic: {
+    overrides: {
+      'style/semi': ['error', 'always'],
+      'style/brace-style': ['error', '1tbs'],
+      'curly': ['error', 'all'],
+      'style/member-delimiter-style': [
+        'error',
+        {
+          multiline: {
+            delimiter: 'semi',
+            requireLast: true,
+          },
+          singleline: {
+            delimiter: 'comma',
+            requireLast: true,
+          },
+          overrides: {
+            interface: {
+              multiline: {
+                delimiter: 'semi',
+                requireLast: true,
+              },
+            },
+          },
+        },
+      ],
     },
   },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+  javascript: {
+    overrides: {
+      'new-cap': 'off',
+      'yoda': 'error',
+      'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+      'no-unneeded-ternary': 'error',
+      'no-empty': 'error',
     },
   },
-);
+  jsonc: false,
+  yaml: false,
+  ignores: ['./node_modules', './build', './dist'],
+  rules: {
+    'style/max-len': ['error', { code: 120 }],
+    'no-unused-private-class-members': 'warn',
+    'no-eq-null': 'error',
+    'max-classes-per-file': ['error', 1],
+    'no-undefined': 'warn',
+    'require-await': 'error',
+    'require-atomic-updates': 'error',
+    'ts/array-type': ['error', { default: 'generic' }],
+    'ts/no-namespace': 'off',
+    'ts/consistent-type-imports': 'off',
+    'ts/consistent-type-definitions': 'off',
+  },
+});
