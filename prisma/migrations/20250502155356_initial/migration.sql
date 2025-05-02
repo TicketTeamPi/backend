6540759f3046d6cdab1058edd85ffe04f909e191
GabrielFogo <gabrielcostal164@gmail.com>
2025-05-02 12:54:19 -0300

    refactor: remove email field

--- prisma/migrations/20250502155356_initial/migration.sql
+++ prisma/migrations/20250502155356_initial/migration.sql
@@ -3,7 +3,6 @@ CREATE TABLE `Enterprise` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `cnpj` VARCHAR(191) NOT NULL,
-    `email` VARCHAR(191) NULL,
     `phone` VARCHAR(191) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NULL,
