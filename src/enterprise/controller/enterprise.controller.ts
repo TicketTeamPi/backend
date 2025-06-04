b46c72252e8a9b4f9e636d6bc09e6229265fed1a
Arthur Servidor <86123199+ArthurServidor@users.noreply.github.com>
2025-06-03 23:00:41 -0300

    retira autenticação de empresa

--- src/enterprise/controller/enterprise.controller.ts
+++ src/enterprise/controller/enterprise.controller.ts
@@ -1,10 +1,8 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { EnterpriseDto } from '../dtos/input/enterprise.dto';
 import { EnterpriseService } from '../services/enterprise.service';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller('enterprise')
-@UseGuards(AuthGuard('jwt'))
 export class EnterpriseController {
   constructor(private readonly _enterpriseService: EnterpriseService) {}
 
