0995e1af3a98c51a7fac70c37b6e007f58fd50d0
Arthur Servidor <86123199+ArthurServidor@users.noreply.github.com>
2025-06-28 00:13:49 -0300

    endpoint que move para outro setor esta feito

--- app/validators/ticket/patch.ts
+++ app/validators/ticket/patch.ts
@@ -4,5 +4,6 @@ export const patchValidator = vine.compile(
   vine.object({
     title: vine.string().minLength(3).maxLength(100).optional(),
     description: vine.string().maxLength(255).optional(),
+    priority: vine.enum(['low', 'medium', 'high']).optional(),
   })
 )
